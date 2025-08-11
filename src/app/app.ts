a2dd26801b19927a186f877363fc591495a269e3
Oskars Lācis <oskars.lacis@lightsoft.lv>
2025-08-11 18:42:37 +0300

    Angular

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { Navbar } from './navbar/navbar';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    Navbar
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('TavsNamdaris');
+}
